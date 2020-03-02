3ce6dfd65aece75b7839783a24675788d3844e45
AustinPotts <pottsevolvements@gmail.com>
2020-03-02 12:25:20 -0500

    Project Configured

---
+++ WaterMyPlants-iOS/WaterMyPlants-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterMyPlants-iOS
+//
+//  Created by Austin Potts on 3/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
