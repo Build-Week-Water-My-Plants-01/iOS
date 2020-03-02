57b5914b3f9f41a74a6b604cb09e9a26166f93ac
AustinPotts <pottsevolvements@gmail.com>
2020-03-02 15:40:18 -0500

    Add Plant View Controller Configured

---
+++ WaterMyPlants-iOS/WaterMyPlants-iOS/View Controllers/PlantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantTableViewCell.swift
+//  WaterMyPlants-iOS
+//
+//  Created by Austin Potts on 3/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
